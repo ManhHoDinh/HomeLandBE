f8120b35e7a497560765723e298e84c941d9b09f
Daiduong1593572468 <daiduong35578@gmail.com>
2024-06-02 22:41:43 +0700

    chore: Add API endpoint for retrieving a floor by ID

--- src/owner/dto/update-owner.dto.ts
+++ src/owner/dto/update-owner.dto.ts
@@ -1,17 +1,17 @@
-import { ApiProperty, OmitType, PartialType } from "@nestjs/swagger";
-import { CreateFloorDto } from "./create-owner.dto";
-import { IsNumberString, IsOptional, IsString } from "class-validator";
-import { Transform } from "class-transformer";
-export class UpdateFloorDto extends PartialType(
-        OmitType(CreateFloorDto, [
-            "apartmentIds",
-            "max_apartment"
+// import { ApiProperty, OmitType, PartialType } from "@nestjs/swagger";
+// import { CreateFloorDto } from "./create-owner.dto";
+// import { IsNumberString, IsOptional, IsString } from "class-validator";
+// import { Transform } from "class-transformer";
+// export class UpdateFloorDto extends PartialType(
+//         OmitType(CreateFloorDto, [
+//             "apartmentIds",
+//             "max_apartment"
            
-        ] as const),
-) {
-    @ApiProperty()
-    @IsNumberString()
-    @IsOptional()
-    max_apartment: number
+//         ] as const),
+// ) {
+//     @ApiProperty()
+//     @IsNumberString()
+//     @IsOptional()
+//     max_apartment: number
 
-}
+// }
