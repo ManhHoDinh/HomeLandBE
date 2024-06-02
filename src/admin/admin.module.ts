18dd78eabe2e5f2819755be771aae1c90cd42b0a
ManhHoDinh <21522327@gm.uit.edu.vn>
2024-06-02 22:55:09 +0700

    change contract - refactor code

--- src/admin/admin.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from "@nestjs/common";
-import { AdminSubscriber } from "./admin.sub";
-
-@Module({
-    providers: [AdminSubscriber],
-})
-export class AdminModule { }
--- src/monthly_invoice/monthly_invoice.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { MonthlyInvoiceService } from './monthly_invoice.service';
-import { MonthlyInvoiceController } from './monthly_invoice.controller';
-
-@Module({
-  controllers: [MonthlyInvoiceController],
-  providers: [MonthlyInvoiceService],
-})
-export class MonthlyInvoiceModule {}
