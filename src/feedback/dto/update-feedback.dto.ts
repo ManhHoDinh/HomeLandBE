9a018150371a2c02fa5494a5f00ec949d47cbd70
ManhHoDinh <21522327@gm.uit.edu.vn>
2024-06-02 22:51:35 +0700

    change contract - refactor code

--- src/feedback/dto/update-feedback.dto.ts
+++
@@ -1,7 +0,0 @@
-import { OmitType, PartialType } from "@nestjs/swagger";
-import { CreateFeedbackDto } from "./create-feedback.dto";
-
-export class UpdateFeedbackDto extends PartialType(OmitType(CreateFeedbackDto, [
-  "resident_id",
-  "service_id"
-] as const),) {}
--- src/monthly_invoice/dto/update-monthly_invoice.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateMonthlyInvoiceDto } from './create-monthly_invoice.dto';
-
-export class UpdateMonthlyInvoiceDto extends PartialType(CreateMonthlyInvoiceDto) {}
